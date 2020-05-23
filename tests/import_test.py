e35b47005a6ba4cfcd72c2a564b60528c069f5af
zsxoff <zsxoff@gmail.com>
2020-05-23 18:27:23 +0400

    add smoketest: import all modules

---
+++ tests/import_test.py
@@ -0,0 +1,23 @@
+"""Smoke tests."""
+
+
+def import_all_test():
+    """Import all modules."""
+    import black
+    import colorama
+    import flake8
+    import isort
+    import numpy
+    import pipreqs
+    import psutil
+    import pydocstyle
+    import pylint
+    import pytest
+    import requests
+    import rope
+    import tabulate
+    import toml
+    import tqdm
+    import yaml
+
+    return 0
